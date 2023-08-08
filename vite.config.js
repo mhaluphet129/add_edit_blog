b4025c391f46ac9a5ee1aca4009cad8924e0ff26
Ariel Llanita <llanitaariel@gmail.com>
2023-08-08 10:50:06 +0800

    automate setting for api url

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,21 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 
-const localUrl = "https://dev.api.visitour.ph";
-const mode = "dev"; // prod or dev
-export default defineConfig({
+const prodApi = "https://api.visitour.ph";
+const stagingApi = "https://dev.api.visitour.ph";
+
+const config = {
   plugins: [react()],
   define: {
-    appUrl: JSON.stringify(
-      mode == "dev" ? localUrl : "https://api.visitour.ph"
-    ),
+    appUrl: JSON.stringify(prodApi),
   },
+};
+
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    return { ...config, define: { appUrl: JSON.stringify(stagingApi) } };
+  } else {
+    // command === 'build'
+    return { ...config };
+  }
 });
